03d3a8c860acd3d3ba6f46f91d02765d5144f0d0
Mandy McCabe <91921676+mandymccabe@users.noreply.github.com>
2022-01-11 21:43:35 -0600

    completed audit

--- Challenge/PyPoll.py
+++ Challenge/PyPoll.py
@@ -8,6 +8,19 @@ file_to_load = os.path.join("election_results.csv")
 #create a file to save to and open and write to it
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+#total votes variable
+total_votes= 0
+
+#create candidate list
+candidate_options = []
+
+#create vote count dictionary
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage= 0 
+
 #open the csv file
 with open(file_to_load) as election_data:
 
@@ -15,9 +28,48 @@ with open(file_to_load) as election_data:
 #read the file object with the reader function
     file_reader = csv.reader(election_data)
 
+    #read the header row
+    headers = next(file_reader)
+
 #print each row in the CSV file
-    for roe in next(file_reader):
-        print(row)
+    for row in file_reader:
+        #2. add ot the total vote count
+        total_votes += 1
+        
+        candidate_name = row[2]
+    
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name] += 1
+
+       # Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count= votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+ 
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+#print summary
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+ 
+
+   
 
 
 
@@ -25,12 +77,7 @@ with open(file_to_load) as election_data:
 
 
 
-
-
-#1. Total number of votes cast
-#2. List of candidates
 #3. % votes for each candidate
-#4. total votes for each candidate
 #5. winner
 
 
